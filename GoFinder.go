bb7d99a30177eb6153cffe81e220a36c5f8772f7
OUsteventhomas <OUsteventhomas@hotmail.com>
2017-02-14 09:23:57 -0600

    Update GoFinder.go
    Changed to include specified list of keywords to look for in a document.

--- GoFinder.go
+++ GoFinder.go
@@ -11,10 +11,10 @@ import (
 
 func GetDocumentContent(filename string) (documentContent string) {
 
-    if !strings.HasSuffix(filename,"docx") {
-        fmt.Println("Not a valid docx file")
-        return
-    }
+	if !strings.HasSuffix(filename,"docx") {
+		fmt.Println("Not a valid docx file")
+		return
+	}
 	r, err := zip.OpenReader(filename)
 	if err != nil {
 		log.Fatal(err)
@@ -50,7 +50,7 @@ func GetDocumentContent(filename string) (documentContent string) {
 	}
 	defer rc.Close()
 
-	documentContent = string(data)
+	documentContent = strings.ToLower(string(data)) //right here do a "to lower" for search matching ease
 	return
 }
 
@@ -63,29 +63,45 @@ func SearchKeywords(data string, keywords map[string]bool) {
 }
 
 func main() {
-	if len(os.Args) < 3 {
+	fmt.Printf("\n")
+	if len(os.Args) < 2 {
 		fmt.Println("\n Usage: GoFinder <filePath> <keyword1> [<keyword2>...n]" +
-			"\n e.g. GoFinder /tmp/sample.docx C++ Java\n")
+			"\n e.g. GoFinder C:\\Users\\joe.smith\\Desktop\\sample_contract.docx\n")
 		return
 	}
 
 	keywords := make(map[string]bool)
 
-	for _, keyword := range os.Args[2:] {
+	// keywords to check for, left in this fashion for readability
+	// all keywords should be in lowercase to match text from docx file
+	var text_check [10]string
+	text_check[0] = "expiration"
+	text_check[1] = "confidentiality"
+	text_check[2] = "background"
+	text_check[3] = "pricing"
+	text_check[4] = "executive summary"
+	text_check[5] = "assumptions"
+	text_check[6] = "scope of services"
+	text_check[7] = "pricing"
+	text_check[8] = "travel"
+	text_check[9] = "schedule"
+
+
+	for _, keyword := range text_check {
 		keywords[keyword] = false
 	}
 
-    filePath := os.Args[1]
+	filePath := os.Args[1]
 
-    documentContent := GetDocumentContent(filePath)
-    if len(documentContent) == 0 {
-        fmt.Println("Empty document")
-        return
-    }
+	documentContent := GetDocumentContent(filePath)
+	if len(documentContent) == 0 {
+		fmt.Println("Empty document")
+		return
+	}
 
 	SearchKeywords(documentContent, keywords)
 
 	for keyword, exists := range keywords {
-		fmt.Println(keyword, exists)
+		fmt.Println(exists, "\t", keyword)
 	}
 }
